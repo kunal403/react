9d328914af81ad834488a732c6997f0d4b3d5c31
Kunal kumar <kunalkr3975@gmail.com>
2024-10-18 00:28:45 +0530

    yup

--- 12appWrite/src/confi/confi.js
+++ 12appWrite/src/confi/confi.js
@@ -4,6 +4,7 @@ const confi = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-}
+};
+
 
 export default confi
